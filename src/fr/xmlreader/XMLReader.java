5124c2de30e57f36c8618b8a157abb6429ade15c
celbax95 <loicmace95@gmail.com>
2019-04-29 22:21:24 +0200

    initial commit ajout de la classe XMLReader

--- src/fr/xmlreader/XMLReader.java
+++ src/fr/xmlreader/XMLReader.java
@@ -1,25 +1,6 @@
 package fr.xmlreader;
 
 public class XMLReader {
-
-	/* VARIABLES */
-
-	/* CONSTRUCTORS */
-
-	// Empty
 	public XMLReader() {
-
 	}
-
-//	// Full
-//	public XMLReader() {
-//
-//	}
-//
-//	public XMLReader() {
-//
-//	}
-
-	/* METHODS */
-
 }
