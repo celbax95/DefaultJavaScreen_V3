b68be661f200639972577c7c1b4427d035c917ba
celbax95 <loicmace95@gmail.com>
2019-05-13 16:36:28 +0200

    [MOUSE] Avancement mouseListner

--- src/fr/mouse/MouseHolder.java
+++ src/fr/mouse/MouseHolder.java
@@ -1,28 +1,49 @@
 package fr.mouse;
 
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
-public class MouseHolder implements MouseListener {
+import javax.swing.event.MouseInputListener;
 
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
+public class MouseHolder implements MouseInputListener, MouseMotionListener, MouseWheelListener {
+
+	private static MouseManager mouseManager;
+
+	static {
+		mouseManager = MouseManager.getInstance();
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
+	public void mouseClicked(MouseEvent e) {
 	}
 
 	@Override
-	public void mouseExited(MouseEvent arg0) {
+	public void mouseDragged(MouseEvent e) {
 	}
 
 	@Override
-	public void mousePressed(MouseEvent arg0) {
+	public void mouseEntered(MouseEvent e) {
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent arg0) {
+	public void mouseExited(MouseEvent e) {
 	}
 
+	@Override
+	public void mouseMoved(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+	}
 }
--- src/fr/mouse/MouseManager.java
+++ src/fr/mouse/MouseManager.java
@@ -1,5 +1,103 @@
 package fr.mouse;
 
+import fr.util.point.Point;
+
 public class MouseManager {
+
+	/**
+	 * static Singleton instance.
+	 */
+	private static volatile MouseManager instance;
+
+	private Object mousePressed;
+
+	private Object mouseReleased;
+
+	private Object mouseMoved;
+
+	private Point mousePos;
+
+	private Point lastPressedPos;
+
+	private Point lastReleasedPos;
+
+	private Boolean[] buttons;
+
+	/**
+	 * Private constructor for singleton.
+	 */
+	private MouseManager() {
+		this.mousePressed = new Object();
+		this.mouseReleased = new Object();
+		this.mouseMoved = new Object();
+
+		this.buttons = new Boolean[3];
+	}
+
+	public Boolean[] getButtons() {
+		return this.buttons;
+	}
+
+	public Point getLastPressedPos() {
+		return this.lastPressedPos;
+	}
+
+	public Point getLastReleasedPos() {
+		return this.lastReleasedPos;
+	}
+
+	public Object getMouseMoved() {
+		return this.mouseMoved;
+	}
+
+	public Point getMousePos() {
+		return this.mousePos;
+	}
+
+	public Object getMousePressed() {
+		return this.mousePressed;
+	}
+
+	public Object getMouseRealeased() {
+		return this.mouseReleased;
+	}
+
+	public void setButtons(Boolean[] buttons) {
+		this.buttons = buttons;
+	}
+
+	public void setLastClickPos(Point lastClickPos) {
+		this.lastPressedPos = lastClickPos;
+	}
+
+	public void setLastPressedPos(Point lastPressedPos) {
+		this.lastPressedPos = lastPressedPos;
+	}
+
+	public void setLastReleasedPos(Point lastReleasedPos) {
+		this.lastReleasedPos = lastReleasedPos;
+	}
+
+	public void setMousePos(Point mousePos) {
+		this.mousePos = mousePos;
+		synchronized (this.mouseMoved) {
+			this.mouseMoved.notifyAll();
+		}
+	}
+
+	/**
+	 * Return a singleton instance of MouseManager.
+	 */
+	public static MouseManager getInstance() {
+		// Double lock for thread safety.
+		if (instance == null) {
+			synchronized (MouseManager.class) {
+				if (instance == null) {
+					instance = new MouseManager();
+				}
+			}
+		}
+		return instance;
+	}
 
 }
