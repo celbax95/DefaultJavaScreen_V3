ff8807883808aea4f1b100f6036aca9764206e63
celbax95 <loicmace95@gmail.com>
2019-05-21 14:10:22 +0200

    [XML_MANAGER] fix #4 (1/2) - Documentation de la classe DataFilesManager

--- src/fr/datafilesmanager/DatafilesManager.java
+++ src/fr/datafilesmanager/DatafilesManager.java
@@ -6,6 +6,12 @@ import java.util.Map;
 
 import fr.xmlmanager.XMLManagerDOM;
 
+/**
+ * Gestionnaire des fichiers de configuration
+ *
+ * @author Loic.MACE
+ *
+ */
 public class DatafilesManager {
 
 	private static boolean initialized = false;
@@ -15,14 +21,22 @@ public class DatafilesManager {
 	 */
 	private static volatile DatafilesManager instance;
 
+	// Accesseur aux fichiers de configuration
 	private XMLManager xmlReader;
 
+	// Fichiers sauvegardes
 	private Map<String, String> files;
 
 	private DatafilesManager() {
 		this.files = new HashMap<>();
 	}
 
+	/**
+	 * Ajoute un fichier au gestionnaire
+	 *
+	 * @param name : nom du fichier
+	 * @param path : chemin (path) du fichier
+	 */
 	public void addFile(String name, String path) {
 
 		File file = new File(path);
@@ -35,6 +49,13 @@ public class DatafilesManager {
 		}
 	}
 
+	/**
+	 *
+	 * Recupere un document a partir de la liste de fichiers sauvegardes
+	 *
+	 * @param name : nom du fichier
+	 * @return : le document (racine) du fichier de configuration
+	 */
 	public Object getDocument(String name) {
 		try {
 			return this.xmlReader.getDocument(this.files.get(name));
@@ -45,6 +66,13 @@ public class DatafilesManager {
 		return null; // Inaccessible
 	}
 
+	/**
+	 * Recupere l'extension d'un fichier
+	 *
+	 * @param file : fichier a analyser
+	 *
+	 * @return l'extension du fichier / une chaine vide si le fichier est invalide
+	 */
 	public String getExtension(File file) {
 
 		if (file != null && file.exists()) {
@@ -57,6 +85,11 @@ public class DatafilesManager {
 
 	}
 
+	/**
+	 * Recupere la totalitee des fichiers sauvegardes
+	 *
+	 * @return une Map qui lie le nom du fichier a son chemin (path) d'acces
+	 */
 	public Map<String, String> getFiles() {
 		Map<String, String> m = new HashMap<>();
 
@@ -65,12 +98,26 @@ public class DatafilesManager {
 		return m;
 	}
 
+	/**
+	 * Recupere le xmlReader permettant d'acceder aux informations des fichiers de
+	 * configuration
+	 *
+	 * @return
+	 * @throws RuntimeException
+	 */
 	public XMLManager getXmlReader() throws RuntimeException {
 		if (!initialized)
 			throw new RuntimeException("Appeler la methode init() pour initialiser DatafilesManager");
 		return this.xmlReader;
 	}
 
+	/**
+	 * Methode a appeler avant utilisation
+	 *
+	 * Affecte un Accesseur de XML au Gestionnaire
+	 *
+	 * @param xmlReader : accesseur de XML a affecter
+	 */
 	public void init(XMLManager xmlReader) {
 		this.xmlReader = xmlReader;
 		initialized = true;
