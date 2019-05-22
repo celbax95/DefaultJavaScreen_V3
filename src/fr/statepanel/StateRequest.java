ae63bc705c6e79c2fe6514bb058bed8fc3a1eeac
celbax95 <loicmace95@gmail.com>
2019-05-22 13:37:58 +0200

    [MASTER] fix #7 (7/7) - Documentation pour l'erreur StateRequest

--- src/fr/statepanel/StateRequest.java
+++ src/fr/statepanel/StateRequest.java
@@ -1,16 +1,32 @@
 package fr.statepanel;
 
+/**
+ * Requete de changement d'etat
+ *
+ * @author Loic.MACE
+ *
+ */
 public class StateRequest extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
 	private final String state;
 
+	/**
+	 * Constructeur
+	 *
+	 * @param newState : etat de substitution
+	 */
 	public StateRequest(String newState) {
 		super("Request de changement de state vers : " + newState);
 		this.state = newState;
 	}
 
+	/**
+	 * Recupere l'etat de substitution
+	 * 
+	 * @return l'etat de substitution
+	 */
 	public String getState() {
 		return this.state;
 	}
