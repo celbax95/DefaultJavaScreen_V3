f978d33de92ea88dff569d1f57d686402ac12234
celbax95 <loicmace95@gmail.com>
2019-05-22 09:33:20 +0200

    [MASTER] fix #7 (1/7) - Documentation pour l'interface repainter

--- src/fr/statepanel/Repainter.java
+++ src/fr/statepanel/Repainter.java
@@ -2,14 +2,40 @@ package fr.statepanel;
 
 import javax.swing.JPanel;
 
+/**
+ * Les objets implementant cette interface sont utilises pour repeindre l'ecran
+ *
+ * @author Loic.MACE
+ *
+ */
 public interface Repainter extends Runnable {
+
+	/**
+	 * Interrompt le thread du repainter
+	 */
 	void interrupt();
 
+	/**
+	 * Demande de repaint
+	 */
 	void repaint();
 
+	/**
+	 * Initialisation du panel a repaint
+	 *
+	 * @param panel : panel a repaint
+	 */
 	void setPanel(JPanel panel);
 
+	/**
+	 * Initialisation du rythme de repaint
+	 *
+	 * @param rate : rythme de repaint
+	 */
 	void setRate(int rate);
 
+	/**
+	 * Lance le Thread de repaint
+	 */
 	void start();
 }
