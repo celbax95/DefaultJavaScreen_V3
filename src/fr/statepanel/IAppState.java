5881e49cd8c3255671d70e1e5f8218552db42293
celbax95 <loicmace95@gmail.com>
2019-05-22 13:36:44 +0200

    [MASTER] fix #7 (5/7) - Documentation pour l'interface IAppState

--- src/fr/statepanel/IAppState.java
+++ src/fr/statepanel/IAppState.java
@@ -3,16 +3,53 @@ package fr.statepanel;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+/**
+ * Interface representant un etat
+ *
+ * @author Loic.MACE
+ *
+ */
 public interface IAppState {
+	/**
+	 * Methode appelee lors d'un repaint
+	 *
+	 * @param g : Objet servant a afficher
+	 * @throws StateRequest : changement d'etat
+	 */
 	void draw(Graphics2D g) throws StateRequest;
 
+	/**
+	 * Recupere la couleur de fond
+	 *
+	 * @return : la couleur de l'etat
+	 */
 	Color getBackgroundColor();
 
+	/**
+	 * Recupere le nom de l'etat
+	 *
+	 * @return : le nom de l'etat
+	 */
 	String getName();
 
+	/**
+	 * Recupere le rythme de repaint
+	 *
+	 * @return le rythme de repaint
+	 */
 	int getRepaintRate();
 
+	/**
+	 * Change l'etat actif de l'etat
+	 *
+	 * @param active : true si actif
+	 */
 	void setActive(boolean active);
 
+	/**
+	 * Initialise le repainter
+	 *
+	 * @param repainter : nouveau repainter
+	 */
 	void setRepainter(Repainter repainter);
 }
