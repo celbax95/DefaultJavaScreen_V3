0dff5a889287a20c0fb373513315376a28c8438a
celbax95 <loicmace95@gmail.com>
2019-05-22 09:47:11 +0200

    [MASTER] fix #7 (2/7) - Documentation pour l'interface Statable

--- src/fr/statepanel/Statable.java
+++ src/fr/statepanel/Statable.java
@@ -1,5 +1,17 @@
 package fr.statepanel;
 
+/**
+ * Des etats peuvent être attribuees aux objets implementant cette interface
+ *
+ * @author Loic.MACE
+ *
+ */
 public interface Statable {
+
+	/**
+	 * Initialise l'etat de l'objet implementant Statable
+	 *
+	 * @param appState : etat
+	 */
 	void setState(IAppState appState);
 }
