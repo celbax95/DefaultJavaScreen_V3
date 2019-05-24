2e9edd659d821b6ce67334a000b4f374b72fdc9e
celbax95 <loicmace95@gmail.com>
2019-05-24 13:49:05 +0200

    [MENU] fix #16 - Creation de l'interface widget

--- src/fr/menu/Widget.java
+++ src/fr/menu/Widget.java
@@ -1,5 +1,7 @@
 package fr.menu;
 
+import java.awt.Graphics2D;
+
 /**
  * Un widget est un element affiche par le menu
  *
@@ -8,4 +10,33 @@ package fr.menu;
  */
 public interface Widget {
 
+	/**
+	 * Dessine le widget grâce a l'objet Graphics2D
+	 *
+	 * @param g : l'objet Graphics2D
+	 */
+	void draw(Graphics2D g);
+
+	/**
+	 * Recupere l'etat selectionne du widget
+	 *
+	 * @return l'etat selectionne du widget
+	 */
+	boolean isSelected();
+
+	/**
+	 * Selectionne le widget
+	 */
+	void select();
+
+	/**
+	 *
+	 * Initialise les signaux
+	 *
+	 * @param clickSignal     : signal emi lors d'un clic souris
+	 * @param moveSignal      : signal emi lorsque la souris est bougee
+	 * @param mouseWeelSignal : signal emi lorsque la molette est actionnee
+	 * @param keyboardSignal  : signal emi lorsque le clavier est actionne
+	 */
+	void setSignals(Object clickSignal, Object moveSignal, Object mouseWeelSignal, Object keyboardSignal);
 }
