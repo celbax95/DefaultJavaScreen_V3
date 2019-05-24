386c4134a575313ab13d2e41fdb2f90c76bce9b0
celbax95 <loicmace95@gmail.com>
2019-05-24 13:49:42 +0200

    [MENU] fix #18 - Creation de la premiere version de la classe Page

--- src/fr/menu/Page.java
+++ src/fr/menu/Page.java
@@ -1,5 +1,8 @@
 package fr.menu;
 
+import java.awt.Graphics2D;
+import java.util.List;
+
 /**
  * Page du menu
  *
@@ -7,5 +10,17 @@ package fr.menu;
  *
  */
 public class Page {
+
+	public List<Widget> widgets;
+
+	public void addWidget(Widget widget) {
+		this.widgets.add(widget);
+	}
+
+	public void draw(Graphics2D g) {
+		for (Widget widget : this.widgets) {
+			widget.draw(g);
+		}
+	}
 
 }
