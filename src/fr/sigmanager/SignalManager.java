7308ebd6c161ef736d999b52da4b69ea20226441
celbax95 <loicmace95@gmail.com>
2019-06-19 15:50:39 +0200

    [SIG_MANAGER] Ajout des gestionnaires de signaux et de threads

---
+++ src/fr/sigmanager/SignalManager.java
@@ -0,0 +1,278 @@
+package fr.sigmanager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Gestionnaire de signal
+ *
+ * @author Loïc
+ *
+ */
+public class SignalManager {
+
+	/**
+	 * static Singleton instance.
+	 */
+	private static volatile SignalManager instance;
+
+	// Map : key = signalName / value = signal
+	private Map<String, Object> signals;
+
+	// Map : key = signalName / value = liste des threads lies
+	private Map<String, List<String>> linkedThreads;
+
+	// Signaux verouilles
+	private List<String> lockedSignals;
+
+	/**
+	 * Private constructor for singleton.
+	 */
+	private SignalManager() {
+		this.signals = new HashMap<>();
+		this.linkedThreads = new HashMap<>();
+		this.lockedSignals = new ArrayList<>();
+	}
+
+	/**
+	 * Lie un thread a un signal
+	 *
+	 * @param sigName    : nom du signal
+	 * @param threadName : nom du thread a lie
+	 */
+	public void addLinkedThread(String sigName, String threadName) {
+		synchronized (this.signals) {
+			if (this.signals.containsKey(sigName)) {
+				synchronized (this.linkedThreads) {
+
+					ThreadManager threadManager = ThreadManager.getInstance();
+
+					if (threadManager.contains(threadName)) {
+
+						this.linkedThreads.get(sigName).add(threadName);
+						threadManager.addLinkedSignal(threadName, sigName);
+
+					} else
+						throw new RuntimeException("Le thread n'existe pas");
+
+				}
+			} else
+				throw new RuntimeException("Le signal n'existe pas");
+		}
+	}
+
+	/**
+	 * Ajoute un signal
+	 *
+	 * @param sigName : nom du signal
+	 * @throws RuntimeException
+	 */
+	public void addSignal(String sigName) throws RuntimeException {
+		synchronized (this.signals) {
+
+			if (!this.signals.containsKey(sigName)) {
+
+				this.signals.put(sigName, new Object());
+
+				synchronized (this.linkedThreads) {
+					this.linkedThreads.put(sigName, new ArrayList<>());
+				}
+
+			} else
+				throw new RuntimeException("Le signal existe deja");
+		}
+	}
+
+	/**
+	 * recupere un signal
+	 *
+	 * @param sigName : nom du signal
+	 * @return : le signal recupere
+	 * @throws RuntimeException
+	 */
+	public Object get(String sigName) throws RuntimeException {
+		if (this.signals.containsKey(sigName))
+
+			return this.signals.get(sigName);
+
+		else
+			throw new RuntimeException("Le signal n'existe pas");
+	}
+
+	/**
+	 * Recupere le nom des thread lies au signal
+	 *
+	 * @param sigName : nom du signal
+	 * @return la liste de nom des threads lies
+	 * @throws RuntimeException
+	 */
+	public List<String> getLinkedThreadsName(String sigName) throws RuntimeException {
+		if (this.signals.containsKey(sigName)) {
+			if (this.linkedThreads.containsKey(sigName))
+				return this.linkedThreads.get(sigName);
+			else
+				throw new RuntimeException("Le signal n'a aucun threads lie");
+		} else
+			throw new RuntimeException("Le signal n'existe pas");
+	}
+
+	/**
+	 * Test le sttatut "vide" de la map
+	 */
+	public boolean isEmpty() {
+		return this.signals.isEmpty();
+	}
+
+	/**
+	 * Test si un signal est verouille
+	 *
+	 * @param sigName : nom du signal
+	 *
+	 * @return true si le signal est verouille
+	 */
+	public boolean isLocked(String sigName) {
+		return this.lockedSignals.contains(sigName);
+	}
+
+	/**
+	 * Verouille un signal, il ne sera pas supprime par un reset
+	 *
+	 * @param sigName : signal a verouiller
+	 */
+	public void lock(String sigName) {
+		synchronized (this.signals) {
+			if (this.signals.containsKey(sigName)) {
+				synchronized (this.lockedSignals) {
+					if (!this.lockedSignals.contains(sigName)) {
+
+						this.lockedSignals.add(sigName);
+
+					} else
+						throw new RuntimeException("Le signal " + sigName + " est deja verouille");
+				}
+			} else
+				throw new RuntimeException("Le signal " + sigName + " n'existe pas");
+		}
+	}
+
+	/**
+	 * Supprime le signal et les threads associes (version synchronisee)
+	 *
+	 * @param sigName : nom du signal
+	 * @throws RuntimeException : le signal n'existe pas
+	 */
+	public void remove(String sigName) throws RuntimeException {
+		synchronized (this.signals) {
+			this.removeUnsync(sigName);
+		}
+	}
+
+	/**
+	 * Supprime le lien entre un signal et un thread
+	 *
+	 * @param sigName    : nom du signal
+	 * @param threadName : nom du thread lie
+	 */
+	public void removeLinkedThread(String sigName, String threadName) {
+		synchronized (this.signals) {
+			synchronized (this.linkedThreads) {
+				this.removeLinkedThreadUnsync(sigName, threadName);
+			}
+		}
+	}
+
+	/**
+	 * Supprime le lien entre un signal et un thread
+	 *
+	 * @param sigName    : nom du signal
+	 * @param threadName : nom du thread lie
+	 */
+	private void removeLinkedThreadUnsync(String sigName, String threadName) {
+		if (this.signals.containsKey(sigName)) {
+			if (this.linkedThreads.get(sigName).contains(threadName)) {
+
+				this.linkedThreads.get(sigName).remove(threadName);
+				ThreadManager.getInstance().removeLinkedSignal(threadName);
+
+			} else
+				throw new RuntimeException("Le thread n'est pas associe au signal");
+		} else
+			throw new RuntimeException("Le signal n'existe pas");
+	}
+
+	/**
+	 * Supprime le signal et les threads associes (version non synchronisee)
+	 *
+	 * @param sigName : nom du signal
+	 * @throws RuntimeException : le signal n'existe pas
+	 */
+	private void removeUnsync(String sigName) throws RuntimeException {
+		if (this.signals.containsKey(sigName)) {
+
+			ThreadManager threadManager = ThreadManager.getInstance();
+
+			synchronized (threadManager) {
+				for (String threadName : this.linkedThreads.get(sigName)) {
+					this.removeLinkedThreadUnsync(sigName, threadName);
+
+					threadManager.remove(threadName);
+				}
+			}
+
+			this.signals.remove(sigName);
+		} else
+			throw new RuntimeException("Le signal n'existe pas");
+	}
+
+	/**
+	 * Supprime tous les signaux et les threads associes a moins que le signal soit
+	 * verouille
+	 */
+	public void reset() throws RuntimeException {
+		synchronized (this) {
+			if (!this.isEmpty()) {
+
+				for (String sigName : this.signals.keySet()) {
+					if (!this.lockedSignals.contains(sigName)) {
+						this.removeUnsync(sigName);
+					}
+				}
+
+			} else
+				throw new RuntimeException("Il n'y a aucun signal");
+		}
+	}
+
+	/**
+	 * Deverouille un signal, il sera supprime lors d'un reset
+	 *
+	 * @param sigName : nom du signal
+	 */
+	public void unlock(String sigName) {
+		synchronized (this.lockedSignals) {
+			if (this.lockedSignals.contains(sigName)) {
+
+				this.lockedSignals.remove(sigName);
+
+			} else
+				throw new RuntimeException("Le signal " + sigName + " n'est pas verouille");
+		}
+	}
+
+	/**
+	 * Return a singleton instance of SignalManager.
+	 */
+	public static SignalManager getInstance() {
+		// Double lock for thread safety.
+		if (instance == null) {
+			synchronized (SignalManager.class) {
+				if (instance == null) {
+					instance = new SignalManager();
+				}
+			}
+		}
+		return instance;
+	}
+}
---
+++ src/fr/sigmanager/ThreadManager.java
@@ -0,0 +1,234 @@
+package fr.sigmanager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Gestionnaire de thread
+ *
+ * @author Loïc
+ *
+ */
+public class ThreadManager {
+
+	/**
+	 * static Singleton instance.
+	 */
+	private static volatile ThreadManager instance;
+
+	// Map : key = threadName / value = thread
+	private Map<String, Thread> threads;
+
+	// Map : key = threadName / value = signalName
+	private Map<String, String> linkedSignals;
+
+	// Threads verouilles
+	private List<String> lockedThreads;
+
+	/**
+	 * Private constructor for singleton.
+	 */
+	private ThreadManager() {
+		this.threads = new HashMap<>();
+		this.linkedSignals = new HashMap<>();
+		this.lockedThreads = new ArrayList<>();
+	}
+
+	/**
+	 * Ajoute un thread au gestionnaire
+	 *
+	 * @param threadName : nom du thread
+	 * @param newThread  : le thread a ajouter
+	 */
+	public void add(String threadName, Thread newThread) {
+		synchronized (this.threads) {
+			if (!this.threads.containsKey(threadName)) {
+				this.threads.put(threadName, newThread);
+			} else
+				throw new RuntimeException("Le thread " + threadName + " existe deja");
+		}
+	}
+
+	/**
+	 * Lie un thread a un signal
+	 *
+	 * @param threadName : nom du thread
+	 * @param sigName    : nom du signal a lie
+	 */
+	protected void addLinkedSignal(String threadName, String sigName) {
+		synchronized (this.threads) {
+			if (this.threads.containsKey(threadName)) {
+				synchronized (this.linkedSignals) {
+
+					if (!this.linkedSignals.containsKey(threadName)) {
+
+						this.linkedSignals.put(threadName, sigName);
+
+					} else
+						throw new RuntimeException(
+								"Le signal " + sigName + " a deja ete ajoute au thread " + threadName);
+				}
+			} else
+				throw new RuntimeException("Le thread " + threadName + " n'existe pas");
+		}
+	}
+
+	/**
+	 * Test si le manager contien un thread
+	 *
+	 * @param threadName : nom du thread
+	 * @return true si le gestionnaire contien le thread, false sinon
+	 */
+	public boolean contains(String threadName) {
+		return this.contains(threadName);
+	}
+
+	/**
+	 * Recupere le nom de chaque thread
+	 *
+	 * @return la liste du nom des threads
+	 */
+	public List<String> getThreadsName() {
+		return new ArrayList<>(this.threads.keySet());
+	}
+
+	/**
+	 * Test si le gestionnaire est vide
+	 *
+	 * @return true si le getsionnaire est vide, false sinon
+	 */
+	public boolean isEmpty() {
+		return this.threads.isEmpty();
+	}
+
+	/**
+	 * Test si un thread est verouille
+	 *
+	 * @param threadName : nom du thread
+	 *
+	 * @return true si le thread est verouille
+	 */
+	public boolean isLocked(String threadName) {
+		return this.lockedThreads.contains(threadName);
+	}
+
+	/**
+	 * Verouille un thread, il ne sera pas supprime par un reset
+	 *
+	 * @param threadName : thread a verouiller
+	 */
+	public void lock(String threadName) {
+		synchronized (this.threads) {
+			if (this.threads.containsKey(threadName)) {
+				synchronized (this.lockedThreads) {
+					if (!this.lockedThreads.contains(threadName)) {
+
+						this.lockedThreads.add(threadName);
+
+					} else
+						throw new RuntimeException("Le thread " + threadName + " est deja verouille");
+				}
+			} else
+				throw new RuntimeException("Le thread " + threadName + " n'existe pas");
+		}
+	}
+
+	/**
+	 * Arrete et supprime un thread du gestionnaire (version synchronisee)
+	 *
+	 * @param threadName : nom du thread
+	 */
+	public void remove(String threadName) {
+		synchronized (this.threads) {
+			this.removeUnsync(threadName);
+		}
+	}
+
+	/**
+	 * Supprime le lien a un thread
+	 *
+	 * @param threadName : nom du thread
+	 */
+	protected void removeLinkedSignal(String threadName) {
+		synchronized (this.linkedSignals) {
+
+			if (this.linkedSignals.containsKey(threadName)) {
+				this.linkedSignals.remove(threadName);
+
+			} else
+				throw new RuntimeException("Le thread " + threadName + " n'a pas de signal lie");
+		}
+	}
+
+	/**
+	 * Arrete et supprime un thread du gestionnaire (version non synchronisee)
+	 *
+	 * @param threadName : nom du thread
+	 */
+	private void removeUnsync(String threadName) {
+		if (this.threads.containsKey(threadName)) {
+			Thread toRemove = this.threads.get(threadName);
+
+			toRemove.interrupt();
+
+			this.threads.remove(threadName);
+
+			if (this.isLocked(threadName)) {
+				this.unlock(threadName);
+			}
+
+			if (this.linkedSignals.containsKey(threadName)) {
+				String linkedSignal = this.linkedSignals.get(threadName);
+				SignalManager.getInstance().removeLinkedThread(linkedSignal, threadName);
+			}
+
+		} else
+			throw new RuntimeException("Le thread " + threadName + " n'existe pas");
+	}
+
+	/**
+	 * Supprime tous les threads a moins qu'il soit verouille
+	 */
+	public void reset() {
+		synchronized (this.threads) {
+			for (String threadName : this.threads.keySet()) {
+				if (!this.lockedThreads.contains(threadName)) {
+					this.removeUnsync(threadName);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Deverouille un thread, il sera supprime lors d'un reset
+	 *
+	 * @param threadName : nom du thread
+	 */
+	public void unlock(String threadName) {
+		synchronized (this.lockedThreads) {
+			if (this.lockedThreads.contains(threadName)) {
+
+				this.lockedThreads.remove(threadName);
+
+			} else
+				throw new RuntimeException("Le thread " + threadName + " n'est pas verouille");
+		}
+	}
+
+	/**
+	 * Return a singleton instance of ThreadManager.
+	 */
+	public static ThreadManager getInstance() {
+		// Double lock for thread safety.
+		if (instance == null) {
+			synchronized (ThreadManager.class) {
+				if (instance == null) {
+					instance = new ThreadManager();
+				}
+			}
+		}
+		return instance;
+	}
+}
